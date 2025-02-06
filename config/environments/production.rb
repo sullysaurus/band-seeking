311317491e486f3f06804e94d9b86a1bc2263c66
Danny Sullivan <daniel-sullivan@pluralsight.com>
2025-02-06 11:39:13 -0500

    configs set for render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.assets.compile = true
+  config.public_file_server.enabled = true
+  config.serve_static_assets = true
 end
